eae0ee6e8d8c31a65b2f3766a747e1e0f1abfa7e
Carlos Barrera <barrera9696@gmail.com>
2020-09-19 15:39:58 -0500

    Fixed issue with Input Field (removed size from props) and removed unnecessary props. Clean state, push before installing urql

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -1,6 +1,7 @@
 import React, { InputHTMLAttributes } from 'react'
 import { useField } from 'formik';
 import { FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/core';
+import { __ApiPreviewProps } from 'next/dist/next-server/server/api-utils';
  
 
 // specifying type for InputField 
@@ -8,11 +9,15 @@ import { FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/core
 // that a regular input field would take  
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
     label: string,
-    placeholder: string
     name: string,
 };
 
-export const InputField: React.FC<InputFieldProps> = (props) => {
+export const InputField: React.FC<InputFieldProps> = ({
+    label, 
+    size: _, // Removes size from props and assign it to _ (unused variables)
+    ...props
+    }
+    ) => {
     // setting field here allows us to pass 
     // all the other functions to our Input
     const [field, {error}] = useField(props);
@@ -21,12 +26,12 @@ export const InputField: React.FC<InputFieldProps> = (props) => {
         <FormControl isInvalid={!!error}> 
         <FormLabel 
         htmlFor={field.name}>
-            {props.label}
+            {label}
         </FormLabel>
         <Input 
         {...field}
+        {...props}
         id={field.name} 
-        placeholder={props.placeholder} 
         />
         {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
     </FormControl>
--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Formik, Form, Field} from 'formik'
-import { Button, FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/core'
+import { Box, Button, FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/core'
 import { Wrapper } from '../components/wrapper'
 import { InputField } from '../components/InputField';
 
@@ -14,19 +14,30 @@ const Register: React.FC<registerProps> = ({}) => {
             initialValues={{ username: "", password: "" }}
             onSubmit={(values) => console.log({values})}
     >
-      {({values, handleChange}) => (
+      {({isSubmitting}) => (
             <Form>
                <InputField 
                name="username"
                label="username"
                placeholder="Username"
+               type="text"
                />
+               <Box mt={4}>
+               <InputField 
+               name="password"
+               label="password"
+               placeholder="Password"
+               type="password"
+               />
+                </Box>
                 <Button
                     mt={4}
-                    variantColor="teal"
+                    // pass isSubmitting value from Formik to isLoading from Chakra 
+                    isLoading={isSubmitting}
+                    variantColor="green"
                     type="submit"
                 >
-                    Submit
+                    Register
                 </Button>
             </Form>
       )}
